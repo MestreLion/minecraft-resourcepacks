f9dac3f443d0ec2d3c1d1a627ca0f9dc3f927fec
MestreLion <linux@rodrigosilva.com>
2021-11-15 16:43:46 -0300

    add notes on Minecraft stem colors

--- Different Steams/tint.sh
+++ Different Steams/tint.sh
@@ -11,6 +11,24 @@ declare -A colors=(
 	[melon]='DarkGreen'  # #006400
 	[pumpkin]='Orange'   # #FFA500
 )
+# In-game colors:
+# Blocks.ATTACHED_MELON_STEM, Blocks.ATTACHED_PUMPKIN_STEM: 14731036
+# Blocks.MELON_STEM, Blocks.PUMPKIN_STEM: {
+# 	int i = StemBlock.AGE;
+# 	int j = i * 32;
+# 	int k = 255 - i * 8;
+# 	int l = i * 4;
+# 	return j << 16 | k << 8 | l;
+# }
+# Age from 0 to 7:
+#    65280
+#  2160388
+#  4255496
+#  6350604
+#  8445712
+# 10540820
+# 12635928
+# 14731036  # Same as Attached color
 
 tint=80  # Percent
 assets=assets/minecraft/textures/block
@@ -25,6 +43,15 @@ mcpath=${1:-${mydir}/../vanilla/1.17.1}
 outdir=${2:-${mydir}/${assets}}
 
 indir=${mcpath}/${assets}
+
+color() {
+	local age=$1
+	local r=$((age * 32))
+	local g=$((255 - age * 8))
+	local b=$((age * 4))
+	local i=$((r << 16 | g << 8 | b))
+	printf '#%02X%02X%02X\t%8d\t(%3d, %3d, %3d)\n' $r $g $b $i $r $g $b
+}
 
 apply_tint() {
 	local f=$1
